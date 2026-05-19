567ffd11f2ba0d21a78daa5b886e384ca0680b78
Maqs <m.switon@apfelwerk.de>
2026-05-19 14:18:27 +0200

    Added css declaration

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+// Type declarations for importing CSS/SCSS files in TypeScript
+declare module '*.css';
+declare module '*.scss';
+declare module '*.module.css';
+declare module '*.module.scss';
+
+export {};
